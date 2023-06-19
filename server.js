8a7dc7d13e0abebc5a9a7e019234823b4ace3cdd
Prakash Prajapati <prakash.sp@accubits.com>
2023-06-19 09:53:25 +0530

    first-commit

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const { WebSocketServer } = require('ws');
-const http = require('http');
+// const { WebSocketServer } = require('ws');
+// const http = require('http');
 
-const server = http.createServer();
-const wsServer = new WebSocketServer({ server });
+// const server = http.createServer();
+// const wsServer = new WebSocketServer({ server });
 
-wsServer.on('connection', function (connection) {
-    console.log(`Recieved a new connection.`);
-});
-const port = 8000;
+// wsServer.on('connection', function (connection) {
+//     console.log(`Recieved a new connection.`);
+// });
+// const port = 8000;
 
-server.listen(port, () => {
-    console.log(`WebSocket server is running on port ${port}`);
-});
+// server.listen(port, () => {
+//     console.log(`WebSocket server is running on port ${port}`);
+// });
